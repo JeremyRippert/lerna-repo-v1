bd00c65fd79c96dfb5be86c8086702763e20d68f
jeremyr23 <jeremyr@theodo.fr>
2022-01-17 17:54:38 +0100

    do stuff

---
+++ packages/frontend/src/pages/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { toto } from '@monorepo/shared';
+
+console.log('hey');
+
+const Index = () => {
+  return (
+    <div>
+      <div>Welcome to Admin, {toto}</div>
+    </div>
+  );
+};
+
+export default Index;
